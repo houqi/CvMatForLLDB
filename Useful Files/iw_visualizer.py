1f3a41eaeedb84a0cbc6a055e1ad5742cbecaf57
Giulio Marin <giulio.marin@me.com>
2015-08-25 12:36:24 -0700

    scale image to full range [0,1] for visualization
    - pixel value written in max zoom is the original one
    - moved auxiliary functions on top to be able to call them
    - reformat the code

--- Useful Files/iw_visualizer.py
+++ Useful Files/iw_visualizer.py
@@ -11,55 +11,6 @@ import sys
 import cv2
 import numpy as np
 
-
-################################
-# Check inputs.
-################################
-
-if len(sys.argv) < 2:
-    print "Not enough arguments"
-    sys.exit()
-else:
-    IMG_NAME = str(sys.argv[1])
-
-
-################################
-# Constants.
-################################
-
-SCALE_MIN = 1
-SCALE_MAX = 8
-MIN_SCALE_FOR_LINES = 4
-MIN_SCALE_FOR_TEXT = 5
-
-
-################################
-# Initializations.
-################################
-
-img = cv2.imread(IMG_NAME)
-size = img.shape[0], img.shape[1]
-
-if len(img.shape) == 2:
-    img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-grayScale = True
-for r in range(0, size[0]):
-    for c in range(0, size[1]):
-        if (img[r, c, 0] != img[r, c, 1] or img[r, c, 0] != img[r, c, 2]):
-            grayScale = False
-
-scale = SCALE_MIN
-xMap = np.zeros(size, dtype=np.float32)
-for r in range(0, size[0]):
-    for c in range(0, size[1]):
-        xMap[r, c] = c
-
-yMap = np.zeros(size, dtype=np.float32)
-for r in range(0, size[0]):
-    for c in range(0, size[1]):
-        yMap[r, c] = r
-
-
 ################################
 # External methods.
 ################################
@@ -67,6 +18,8 @@ for r in range(0, size[0]):
 """
 onMouseEvent
 """
+
+
 def onMouseEvent(event, x, y, flags, param):
     global scale, xMap, yMap
     pointX = xMap[y, x]
@@ -96,17 +49,17 @@ def onMouseEvent(event, x, y, flags, param):
     if scaleChanged:
         xBump = ()
         yBump = ()
-        for r in range (0, size[0]):
-            yMap[r, :] = np.round(pointY + (r - y) / np.exp2(scale-1))
-            if (yMap[r, 0] == yMap[r-1, 0]) == False:
+        for r in range(0, size[0]):
+            yMap[r, :] = np.round(pointY + (r - y) / np.exp2(scale - 1))
+            if (yMap[r, 0] == yMap[r - 1, 0]) == False:
                 yBump = yBump + (r,)
-        for c in range (0, size[1]):
-            xMap[:, c] = np.round(pointX + (c - x) / np.exp2(scale-1))
-            if (xMap[0, c] == xMap[0, c-1]) == False:
+        for c in range(0, size[1]):
+            xMap[:, c] = np.round(pointX + (c - x) / np.exp2(scale - 1))
+            if (xMap[0, c] == xMap[0, c - 1]) == False:
                 xBump = xBump + (c,)
 
         # Add lines for visualization.
-        visualImg = cv2.remap(img, xMap, yMap, cv2.INTER_NEAREST)
+        visualImg = cv2.remap(imgScaled, xMap, yMap, cv2.INTER_NEAREST)
         if scale > MIN_SCALE_FOR_LINES:
             for r in range(1, len(yBump)):
                 visualImg[yBump[r], :] = 63
@@ -118,18 +71,97 @@ def onMouseEvent(event, x, y, flags, param):
             for r in range(1, len(yBump)):
                 for c in range(1, len(xBump)):
                     if grayScale:
-                        cv2.putText(visualImg, (img[yMap[yBump[r]-1, xBump[c]], xMap[yBump[r]-1, xBump[c]], 0]).astype(str), (xBump[c], yBump[r]-3), cv2.FONT_HERSHEY_PLAIN, 0.4 * scale - 1.8, (0, 0, 0), 3)
-                        cv2.putText(visualImg, (img[yMap[yBump[r]-1, xBump[c]], xMap[yBump[r]-1, xBump[c]], 0]).astype(str), (xBump[c], yBump[r]-3), cv2.FONT_HERSHEY_PLAIN, 0.4 * scale - 1.8, (0, 0, 255))
+                        cv2.putText(visualImg,
+                                    (img[yMap[yBump[r] - 1, xBump[c]], xMap[yBump[r] - 1, xBump[c]], 0]).astype(str),
+                                    (xBump[c], yBump[r] - 3), cv2.FONT_HERSHEY_PLAIN, 0.4 * scale - 1.8, (0, 0, 0), 3)
+                        cv2.putText(visualImg,
+                                    (img[yMap[yBump[r] - 1, xBump[c]], xMap[yBump[r] - 1, xBump[c]], 0]).astype(str),
+                                    (xBump[c], yBump[r] - 3), cv2.FONT_HERSHEY_PLAIN, 0.4 * scale - 1.8, (0, 0, 255))
                     else:
-                        cv2.putText(visualImg, (img[yMap[yBump[r]-1, xBump[c]], xMap[yBump[r]-1, xBump[c]], 0]).astype(str), (xBump[c], yBump[r]-3), cv2.FONT_HERSHEY_PLAIN, 0.4 * scale - 1.8, (0, 0, 0), 3)
-                        cv2.putText(visualImg, (img[yMap[yBump[r]-1, xBump[c]], xMap[yBump[r]-1, xBump[c]], 0]).astype(str), (xBump[c], yBump[r]-3), cv2.FONT_HERSHEY_PLAIN, 0.4 * scale - 1.8, (255, 0, 0))
-                        cv2.putText(visualImg, (img[yMap[yBump[r]-1, xBump[c]], xMap[yBump[r]-1, xBump[c]], 1]).astype(str), (xBump[c], yBump[r]-13 - 8 * (scale - 6)), cv2.FONT_HERSHEY_PLAIN, 0.4 * scale - 1.8, (0, 0, 0), 3)
-                        cv2.putText(visualImg, (img[yMap[yBump[r]-1, xBump[c]], xMap[yBump[r]-1, xBump[c]], 1]).astype(str), (xBump[c], yBump[r]-13 - 8 * (scale - 6)), cv2.FONT_HERSHEY_PLAIN, 0.4 * scale - 1.8, (0, 255, 0))
-                        cv2.putText(visualImg, (img[yMap[yBump[r]-1, xBump[c]], xMap[yBump[r]-1, xBump[c]], 2]).astype(str), (xBump[c], yBump[r]-23 -16 * (scale - 6)), cv2.FONT_HERSHEY_PLAIN, 0.4 * scale - 1.8, (0, 0, 0), 3)
-                        cv2.putText(visualImg, (img[yMap[yBump[r]-1, xBump[c]], xMap[yBump[r]-1, xBump[c]], 2]).astype(str), (xBump[c], yBump[r]-23 -16 * (scale - 6)), cv2.FONT_HERSHEY_PLAIN, 0.4 * scale - 1.8, (0, 0, 255))
+                        cv2.putText(visualImg,
+                                    (img[yMap[yBump[r] - 1, xBump[c]], xMap[yBump[r] - 1, xBump[c]], 0]).astype(str),
+                                    (xBump[c], yBump[r] - 3), cv2.FONT_HERSHEY_PLAIN, 0.4 * scale - 1.8, (0, 0, 0), 3)
+                        cv2.putText(visualImg,
+                                    (img[yMap[yBump[r] - 1, xBump[c]], xMap[yBump[r] - 1, xBump[c]], 0]).astype(str),
+                                    (xBump[c], yBump[r] - 3), cv2.FONT_HERSHEY_PLAIN, 0.4 * scale - 1.8, (255, 0, 0))
+                        cv2.putText(visualImg,
+                                    (img[yMap[yBump[r] - 1, xBump[c]], xMap[yBump[r] - 1, xBump[c]], 1]).astype(str),
+                                    (xBump[c], yBump[r] - 13 - 8 * (scale - 6)), cv2.FONT_HERSHEY_PLAIN,
+                                    0.4 * scale - 1.8, (0, 0, 0), 3)
+                        cv2.putText(visualImg,
+                                    (img[yMap[yBump[r] - 1, xBump[c]], xMap[yBump[r] - 1, xBump[c]], 1]).astype(str),
+                                    (xBump[c], yBump[r] - 13 - 8 * (scale - 6)), cv2.FONT_HERSHEY_PLAIN,
+                                    0.4 * scale - 1.8, (0, 255, 0))
+                        cv2.putText(visualImg,
+                                    (img[yMap[yBump[r] - 1, xBump[c]], xMap[yBump[r] - 1, xBump[c]], 2]).astype(str),
+                                    (xBump[c], yBump[r] - 23 - 16 * (scale - 6)), cv2.FONT_HERSHEY_PLAIN,
+                                    0.4 * scale - 1.8, (0, 0, 0), 3)
+                        cv2.putText(visualImg,
+                                    (img[yMap[yBump[r] - 1, xBump[c]], xMap[yBump[r] - 1, xBump[c]], 2]).astype(str),
+                                    (xBump[c], yBump[r] - 23 - 16 * (scale - 6)), cv2.FONT_HERSHEY_PLAIN,
+                                    0.4 * scale - 1.8, (0, 0, 255))
         cv2.imshow('Visualizer', visualImg)
 
 
+def imagesc(img):
+    """Scale image values from 0 to 1"""
+
+    imgout = 1.0 * (img - img.min())
+    imgout /= (imgout.max() - imgout.min())
+    return imgout
+
+
+################################
+# Check inputs.
+################################
+
+if len(sys.argv) < 2:
+    print "Not enough arguments"
+    sys.exit()
+else:
+    IMG_NAME = str(sys.argv[1])
+
+
+################################
+# Constants.
+################################
+
+SCALE_MIN = 1
+SCALE_MAX = 8
+MIN_SCALE_FOR_LINES = 4
+MIN_SCALE_FOR_TEXT = 5
+
+
+################################
+# Initializations.
+################################
+
+img = cv2.imread(IMG_NAME)
+imgScaled = imagesc(img)
+size = img.shape[0], img.shape[1]
+
+if size[0] * size[1] > 800000:
+    print("Image too big")
+    sys.exit(0)
+
+if len(img.shape) == 2:
+    img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+grayScale = True
+for r in range(0, size[0]):
+    for c in range(0, size[1]):
+        if (img[r, c, 0] != img[r, c, 1] or img[r, c, 0] != img[r, c, 2]):
+            grayScale = False
+
+scale = SCALE_MIN
+xMap = np.zeros(size, dtype=np.float32)
+for r in range(0, size[0]):
+    for c in range(0, size[1]):
+        xMap[r, c] = c
+
+yMap = np.zeros(size, dtype=np.float32)
+for r in range(0, size[0]):
+    for c in range(0, size[1]):
+        yMap[r, c] = r
 
 ################################
 # Pipeline.
@@ -137,7 +169,7 @@ def onMouseEvent(event, x, y, flags, param):
 
 cv2.namedWindow('Visualizer')
 cv2.setMouseCallback('Visualizer', onMouseEvent)
-visualImg = cv2.remap(img, xMap, yMap, cv2.INTER_NEAREST)
+visualImg = cv2.remap(imgScaled, xMap, yMap, cv2.INTER_NEAREST)
 cv2.imshow('Visualizer', visualImg)
 cv2.moveWindow('Visualizer', 100, 100)
 cv2.waitKey(0)
